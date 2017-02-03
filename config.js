fd67f55fba4cf45adaf5ceed7757e25976835b3b
felipesilvah <felipesilvah@gmail.com>
2017-02-02 20:53:32 -0500

    Nuevo formato por WP Kit App
    Actualización generada por el plugin de WP Kit App

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'latin-cuisine-android',
+		wp_ws_url : 'http://latincuisine.ca/wp-appkit-api/latin-cuisine-android',
+		wp_url : 'http://latincuisine.ca',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_title : 'Latin Cuisine App',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'nWg4rcseLL6a6AFps6dkRrMXs4SnxT7FSh1VWAayUrNKdvDNgxYEo6oq5kgs2E7G',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
